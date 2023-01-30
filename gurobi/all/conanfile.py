b3230bed25c1158551e81c33c520f8f24f632c04
Ivo Hedtke <ivo.hedtke@dbschenker.com>
2023-01-30 16:30:18 +0100

    Store clean download instead of manipulating the folders
    fixes #1

--- gurobi/all/conanfile.py
+++ gurobi/all/conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.files import collect_libs, copy, download, get
+from conan.tools.files import collect_libs, copy, download, get, rmdir, rm
 
 
 class Gurobi(ConanFile):
@@ -13,8 +13,6 @@ class Gurobi(ConanFile):
     topics = ("linear", "programming", "simplex", "solver")
     options = {"fPIC": [True, False]}
     default_options = {"fPIC": True}
-
-    _root_dir = None
 
     def _get_shared_lib_name(self):
         major_version_no_dots = ''.join(self.version.split('.')[:-1])
@@ -33,35 +31,40 @@ class Gurobi(ConanFile):
 
     def source(self):
         version_no_dots = "gurobi" + self.version.replace('.', '')
-        build_folder = "build"
+        build_folder = "buildfolder"
         if self.settings.os == "Linux":
             get(self, **self.conan_data["sources"][self.version][str(self.settings.os)], destination=build_folder)
-            self._root_dir = f"{build_folder}/{version_no_dots}/linux64"
+            content_dir = f"{build_folder}/{version_no_dots}/linux64"
         else:
             download(self, **self.conan_data["sources"][self.version][str(self.settings.os)], filename="grb")
             self.run(f"pkgutil --expand-full grb {build_folder}")
-            self._root_dir = f"{build_folder}/gurobi{self.version}_macos_universal2.component.pkg/Payload/Library/{version_no_dots}/macos_universal2"
-        copy(self, pattern="*", src=f"{self._root_dir}/src/cpp", dst=self.export_sources_folder, keep_path=False)
-        copy(self, pattern="gurobi*.h", src=f"{self._root_dir}/include", dst=self.export_sources_folder, keep_path=False)
+            rm(self, "grb", ".")
+            content_dir = f"{build_folder}/gurobi{self.version}_macos_universal2.component.pkg/Payload/Library/{version_no_dots}/macos_universal2"
+        copy(self, pattern="*", src=f"{content_dir}/src/cpp", dst="src")
+        copy(self, pattern=self._get_shared_lib_name(), src=f"{content_dir}/lib", dst="lib")
+        copy(self, pattern="gurobi*.h", src=f"{content_dir}/include", dst="include")
+        rmdir(self, build_folder)
 
     def export_sources(self):
         self.copy("CMakeLists.txt")
 
-    def build(self):
+    def generate(self):
         # we build only the C++ lib and copy the C lib
         tc = CMakeToolchain(self)
         tc.generate()
+
+    def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
         # header
-        self.copy(pattern=f"{self._root_dir}/include/gurobi*.h", dst="include", keep_path=False)
+        self.copy(pattern="include/gurobi*.h", dst="include", keep_path=False)
         # C++ lib
         self.copy(pattern="libgurobi_c++.a", dst="lib", keep_path=False)
         # C lib
-        self.copy(pattern=f"{self._root_dir}/lib/{self._get_shared_lib_name()}", dst="lib", keep_path=False)
+        self.copy(pattern=f"lib/{self._get_shared_lib_name()}", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = collect_libs(self)
