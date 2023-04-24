9aec4ee0f7465440113094784fa56004281e6953
Ivo Hedtke <ivo.hedtke@dbschenker.com>
2023-04-24 11:25:38 +0200

    Prepare for usage of CI variables

--- gurobi/all/test_package/main.cpp
+++ gurobi/all/test_package/main.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
-int main() {
-    GRBEnv env {};
+int main()
+{
+    const char* envAppName = std::getenv("GRB_APP_NAME");
+    const char* envClientName = std::getenv("GRB_CLIENT_NAME");
+    const char* envExpDate = std::getenv("GRB_EXP_DATE");
+    const char* envSecret = std::getenv("GRB_SECRET");
+    bool haveCIVars { envAppName && envClientName && envExpDate && envSecret };
+
+    GRBEnv env(haveCIVars);
+    if (haveCIVars) {
+        // todo env.set()
+    }
     GRBModel model { env };
 
     auto x = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x");
